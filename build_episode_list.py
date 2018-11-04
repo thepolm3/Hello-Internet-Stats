dce7ea62a1f15e38b5c17587e996a2e4c0b016d1
thepolm3 <thepolm3@gmail.com>
2018-11-04 17:08:22 +0000

    Update requirements
    Security vunerability in requests 2.19

--- build_episode_list.py
+++ build_episode_list.py
@@ -4,11 +4,14 @@ import get_reddit_thread
 
 print('Downloading rss feed')
 get_rss_feed.main()
+print()
 
 print('Parsing rss feed and getting episode data')
 get_episodes.main()
+print()
 
 print('Getting reddit thread')
 get_reddit_thread.main()
+print()
 
 print('Finished')
